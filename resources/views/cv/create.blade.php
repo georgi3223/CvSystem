94ed5498fb93510e4afdc719ad940f80b4280b21
Georgi Nyagolov <g.nikolaev09393@outlook.com>
2023-08-21 11:26:40 +0300

    making the create cv form

--- resources/views/cv/create.blade.php
+++ resources/views/cv/create.blade.php
@@ -4,23 +4,48 @@
 <div class="p-6 bg-white rounded shadow-md md:w-1/2 lg:w-1/3 m-auto">
  <h1 class="text-2xl font-semibold">Create CV</h1> @if(session('success')) <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mt-4">
   {{ session('success') }}
- </div> @endif <form x-data="cvForm()" x-init="init()" class="mt-4"> @csrf <div class="mb-4">
-   <label class="block text-sm font-medium text-gray-700">First Name:</label>
-   <input type="text" x-model="formData.first_name" class="mt-1 px-4 py-2 border rounded w-full" required>
-  </div>
-  <div class="mb-4">
-   <label class="block text-sm font-medium text-gray-700">Last Name:</label>
-   <input type="text" x-model="formData.last_name" class="mt-1 px-4 py-2 border rounded w-full" required>
-  </div>
+ </div> @endif <form x-data="cvForm()" x-init="init()" class="mt-4"> @csrf
+ <div class="mb-4">
+    <label class="block text-sm font-medium text-gray-700">First Name:</label>
+    <input type="text" x-model="formData.first_name" class="mt-1 px-4 py-2 border rounded w-full" required>
+</div>
+<div class="mb-4">
+    <label class="block text-sm font-medium text-gray-700">Last Name:</label>
+    <input type="text" x-model="formData.last_name" class="mt-1 px-4 py-2 border rounded w-full" required>
+</div>
+<div class="mb-4">
+    <label class="block text-sm font-medium text-gray-700"> Date of Birthday:</label>
+    <input type="date" x-model="formData.birth_date" class="mt-1 px-4 py-2 border rounded w-full" required>
+</div>
   <!-- ... Other input fields ... -->
-  <!-- University Section -->
-  <div class="mb-4">
-   <label class="block text-sm font-medium text-gray-700">University:</label>
-   <select name="university_id" x-model="selectedUniversity" class="mt-1 px-4 py-2 border rounded w-full" required>
-    <option value="" selected disabled>Select University</option> @foreach($universities as $university) <option value="{{ $university->id }}">{{ $university->name }}</option> @endforeach
-   </select>
-   <button type="button" @click="showUniversityPopup = true" class="mt-1 px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"> Add New University </button>
-  </div>
+     <!-- University Section -->
+     <div class="mb-4">
+            <label class="block text-sm font-medium text-gray-700">University:</label>
+            <select name="university_id" x-model="selectedUniversity" class="mt-1 px-4 py-2 border rounded w-full" required>
+                <option value="" selected disabled>Select University</option>
+                @foreach($universities as $university)
+                <option value="{{ $university->id }}">{{ $university->name }}</option>
+                @endforeach
+            </select>
+            <button type="button" @click="showUniversityPopup = true" class="mt-1 px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">
+                Add New University
+            </button>
+        </div>
+        
+        <!-- ... Other form inputs ... -->
+        
+        <!-- Technologies Section -->
+        <div class="mb-4">
+            <label class="block text-sm font-medium text-gray-700">Technologies:</label>
+            <select x-model="selectedTechnologies" multiple class="mt-1 px-4 py-2 border rounded w-full" required>
+                @foreach($technologies as $technology)
+                <option value="{{ $technology->id }}">{{ $technology->name }}</option>
+                @endforeach
+            </select>
+            <button type="button" @click="showTechnologyPopup = true" class="mt-1 px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">
+                Add New Technology
+            </button>
+        </div>
   <!-- University Popup -->
   <div x-show="showUniversityPopup" class="fixed inset-0 flex items-center justify-center">
    <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
@@ -33,12 +58,7 @@
     </div>
    </div>
   </div>
-  <!-- Technologies Section -->
-  <div class="mb-4">
-   <label class="block text-sm font-medium text-gray-700">Technologies:</label>
-   <select x-model="selectedTechnologies" multiple class="mt-1 px-4 py-2 border rounded w-full" required> @foreach($technologies as $technology) <option value="{{ $technology->id }}">{{ $technology->name }}</option> @endforeach </select>
-   <button type="button" @click="showTechnologyPopup = true" class="mt-1 px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"> Add New Technology </button>
-  </div>
+
   <!-- Technologies Popup -->
   <div x-show="showTechnologyPopup" class="fixed inset-0 flex items-center justify-center">
    <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
@@ -56,12 +76,25 @@
   </div>
  </form>
 </div>
-
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.3/dist/cdn.min.js"></script>
 <script>
 function cvForm() {
     return {
-        // ... Other data and methods ...
+        formData: {
+            first_name: '',
+            last_name: '',
+            // ... Other form fields ...
+        },
+        selectedUniversity: '',
+        selectedTechnologies: [],
+        newUniversityName: '',
+        newTechnologyName: '',
+        showUniversityPopup: fаlse,
+        showTechnologyPopup: false,
 
+        init() {
+            // Initialize any necessary data or behaviors when the component is mounted
+        },
         openUniversityPopup() {
             this.showUniversityPopup = true;
         },
@@ -71,6 +104,7 @@ function cvForm() {
         },
         addUniversity() {
             if (this.newUniversityName.trim() !== '') {
+                // Perform AJAX request to add the new university
                 axios.post('{{ route("university.store") }}', {
                         name: this.newUniversityName
                     })
@@ -84,60 +118,15 @@ function cvForm() {
                     });
             }
         },
-        openTechnologyPopup() {
-            this.showTechnologyPopup = true;
-        },
-        addTechnology() {
-            if (this.newTechnologyName.trim() !== '') {
-                axios.post('{{ route("technology.store") }}', {
-                        name: this.newTechnologyName
-                    })
-                    .then(response => {
-                        // Update UI and close popup
-                        // Handle the response if needed
-                        this.newTechnologyName = '';
-                        this.showTechnologyPopup = false;
-                    })
-                    .catch(error => {
-                        console.error('Error adding technology:', error);
-                    });
-            }
-        },
-        submitForm() {
-            // Prepare the form data
-            const formData = {
-                first_name: this.formData.first_name,
-                last_name: this.formData.last_name,
-                birth_date: this.formData.birth_date,
-                university_id: this.selectedUniversity,
-                technologies: this.selectedTechnologies, // Array of selected technology IDs
-                // ... Other form fields ...
-            };
-
-            // Perform AJAX request to submit the form data
-            axios.post('/cv/store', formData)
-                .then(response => {
-                    // Display success message or perform any other actions
-                    alert('CV created successfully');
-                    // Optionally, you can reset the form fields or redirect to another page
-                    this.formData = {
-                        first_name: '',
-                        last_name: '',
-                        birth_date: '',
-                        university_id: '',
-                        technologies: '',
-                        // ... Other form fields ...
-                    };
-                    this.selectedUniversity = ''; // Reset selected university
-                    this.selectedTechnologies = []; // Reset selected technologies
-                })
-                .catch(error => {
-                    // Handle errors if needed
-                    console.error('Error creating CV:', error);
-                });
-        }
-        // ... Other methods ...
+        // Define your other methods here
+        // ...
     };
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize Alpine.js components
+    Alpine.data('cvForm', cvForm);
+});
+
 </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'Laravel') }}</title>
-  <!-- Styles -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
   <!-- Scripts -->
-  <script src="{{ asset('js/app.js') }}" defer></script>
+ 
   <script src="https://cdn.tailwindcss.com"></script>
-  <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js"></script>
-  <script src="jquery-3.6.4.min.js"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.3/dist/cdn.min.js"></script>
+
  </head>
  <body class="font-sans antialiased">
   <div class="min-h-screen bg-gray-100">
